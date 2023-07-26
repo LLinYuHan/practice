0ef123941c6905118fea2889aa0cc7cee9452640
LLinYuHan <1057000273@qq.com>
2023-07-26 20:02:09 +0800

    feature: practice-23

---
+++ src/javascript/algorithm/pointer/merge.js
@@ -0,0 +1,27 @@
+/**
+ * 合并有序数组
+ * @param {number[]} nums1 
+ * @param {number} m 
+ * @param {number[]} nums2 
+ * @param {number} n 
+ */
+var merge = function (nums1, m, nums2, n) {
+    let p1 = m - 1, p2 = n - 1;
+    let tail = m + n - 1;
+    let cur = 0;
+    while (p1 >= 0 || p2 >= 0) {
+        if (p1 === -1) {
+            cur = nums2[p2--];
+        }
+        else if (p2 === -1) {
+            cur = nums1[p1--];
+        }
+        else if (nums1[p1] > nums2[p2]) {
+            cur = nums1[p1--];
+        }
+        else {
+            cur = nums2[p2--];
+        }
+        nums1[tail--] = cur;
+    }
+};
